43f806125677a79ce7192233370c172a27a538ae
Ruth3rl <46648485+ruthel@users.noreply.github.com>
2025-11-25 10:36:19 +0100

    MAJ

--- init-db.sql
+++ init-db.sql
@@ -1,13 +1,2 @@
--- Create user if not exists
-DO
-$do$
-BEGIN
-  IF NOT EXISTS (SELECT FROM pg_user WHERE usename = 'daymark') THEN
-    CREATE USER daymark WITH PASSWORD 'DaymarkSecure2024!@#';
-  END IF;
-END
-$do$;
-
--- Grant privileges
-ALTER USER daymark CREATEDB;
-GRANT ALL PRIVILEGES ON DATABASE daymark TO daymark;
+-- Create daymark database
+CREATE DATABASE daymark OWNER daymark;

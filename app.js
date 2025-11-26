e6181b688e1cb510318868ce707786faacd1622b
Ruth3rl <46648485+ruthel@users.noreply.github.com>
2025-11-26 09:18:28 +0100

    MAj des cors dans app pour accepter toutes les origines

--- app.js
+++ app.js
@@ -36,7 +36,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));

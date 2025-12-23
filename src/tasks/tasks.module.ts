8bd4c9e7d9b147cb50380bcd750a0a232f692cdb
Ruthel <balockruthel.cedrickascens@gmail.com>
2025-12-23 10:35:06 +0100

    BUG Fix : notification au project lead quand la tache est terminée

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { EmailService } from '../common/services/email.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [TasksService],
+  providers: [TasksService, EmailService],
   controllers: [TasksController],
   exports: [TasksService]
 })
